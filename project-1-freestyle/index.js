e6af48ba3ed4ca13d92519dfed873fac6ddc8293
VENKATESAN G <96653425+VenkatMastercoder@users.noreply.github.com>
2026-07-18 21:35:00 +0530

    Update index.js

--- project-1-freestyle/index.js
+++ project-1-freestyle/index.js
@@ -7,7 +7,7 @@ const HOST = "0.0.0.0";
 app.get("/", (req, res) => {
   res.status(200).json({
     status: "ok",
-    project: "project-1-freestyles",
+    project: "project-1-freestyle",
     message: "Welcome to Project 1 API",
   });
 });
